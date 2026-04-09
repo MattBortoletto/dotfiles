99026ee658b84abbc3c5f4dc680ecc74a121c407
Matteo Bortoletto <56031078+MattBortoletto@users.noreply.github.com>
2026-04-09 20:01:45 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,4 +6,8 @@ config.quit_when_all_windows_are_closed = false
 config.check_for_updates = true
 config.hide_tab_bar_if_only_one_tab = true
 
+config.color_scheme = "tokyonight_night"
+config.font = wezterm.font("JetBrains Mono")
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+
 return config
